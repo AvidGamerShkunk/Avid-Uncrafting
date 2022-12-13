87a3fbefb405d1a7159261baee27f9d1e8d8cd2a
AvidGamerShkunk <ghostykirby@gmail.com>
2022-12-13 16:42:39 -0600

    initial commit

---
+++ src/main/java/website/avid/avidsmelting/FurnaceMixin.java
@@ -0,0 +1,9 @@
+package website.avid.avidsmelting;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.gui.screens.inventory.FurnaceScreen;
+
+@Mixin(FurnaceScreen.class)
+public class FurnaceMixin {
+}
